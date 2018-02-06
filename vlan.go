d511f8261d20fc09d22871e3a9498ecb97baf9cc
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-02-06 09:52:33 +0300

    writting "interface VlanXXXX"

--- vlan.go
+++ vlan.go
@@ -1,6 +1,7 @@
 package fwsmConfig
 
 import (
+	"fmt"
 	"io"
 	"net"
 )
@@ -19,6 +20,21 @@ func (a VLANs) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a VLANs) Less(i, j int) bool { return a[i].Index < a[j].Index }
 
 func (vlan VLAN) WriteTo(writer io.Writer) error {
+	fmt.Fprintf(writer, "interface Vlan%v\n", vlan.Index)
+	fmt.Fprintf(writer, " nameif %v\n", vlan.Name)
+	fmt.Fprintf(writer, " security-level %v\n", vlan.SecurityLevel)
+	if len(vlan.IPs) > 0 {
+		ip := vlan.IPs[0]
+		attachedNet := vlan.AttachedNetworks[0]
+		if !attachedNet.Contains(ip) {
+			panic("This shouldn't happend")
+		}
+		mask := net.IP(attachedNet.Mask)
+		fmt.Fprintf(writer, " ip address %v %v\n", ip.String(), mask.String())
+	} else {
+		fmt.Fprintf(writer, " no ip address\n")
+	}
+	fmt.Fprintf(writer, "!\n")
 	return nil
 }
 
